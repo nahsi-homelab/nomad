4bc13b9d5d93c67ca14f62fe1dd5388d6f69bc99
Anatoly Laskaris <github_me@nahsi.dev>
2022-06-11 13:33:10 +0300

    Fix mariadb creds

--- services/filerun/db.php
+++ services/filerun/db.php
@@ -1,11 +1,11 @@
 <?php
 $config['db'] = array (
-  'server' => 'mariadb.service.consul;port=3106',
+  'server' => 'mariadb.service.consul',
   'port' => '3106',
   'database' => 'filerun',
   {{- with secret "mariadb/static-creds/filerun" }}
   'username' => '{{ .Data.username }}',
-  'password' => '{{ .Data.passowrd }}',
+  'password' => '{{ .Data.password }}',
   {{- end }}
 );
 $config['url']['detected_root'] = 'https://filerun.nahsi.dev';
